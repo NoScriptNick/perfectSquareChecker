8ecd8b4df01542e7be22d720bede60327ed9cbc5
613668 <beres.613668@swl.k12.oh.us>
2023-11-09 09:09:12 -0500

    dsfg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.lang.Math;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        int num;
+        double squareRoot;
+
+        System.out.println("Enter a number:");
+        num = scan.nextInt();
+        squareRoot = Math.sqrt(num);
+        if (num / squareRoot == squareRoot) {
+            System.out.println(num + " is a perfect square");
+        } else {
+            System.out.println(num + " is not a perfect square");
+        }
     }
 }
